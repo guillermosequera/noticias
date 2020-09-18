155a4f3bb442cee3823ab92715212f71f2875b49
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-18 17:44:50 -0300

    Form and Moduls css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         />
 
         <div className="container white">
-          <h1>Formulario aqui</h1>
+          <Formulario />
         </div>
     </Fragment>
   );
---
+++ src/components/Formulario.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Formlulario.module.css'
+
+const Formulario = () => {
+    return (
+        <div className={`S{styles.buscador} row`}>
+            <div className="col s12 m8 offset-m2">
+                <form>
+                    <h2 className={styles.heading}>Encuentra Noticias por Categorias</h2>
+
+                    <div className="input-field col s12">
+                        <input 
+                            type="submit"
+                            className={`${styles.btn_block} btn-large amber darken-2`}
+                            value="Buscar"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Formulario
