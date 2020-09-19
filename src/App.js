1e7beb74cc35fb0d62129a7748f1f2e69019cd23
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-18 21:05:25 -0300

    querying the API and reading the results

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
@@ -7,6 +7,19 @@ function App() {
 
   //definir la categoria y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState('');
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ch&category=${categoria}&apiKey=f908b5181c8b42009a48e826782c49a7`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
